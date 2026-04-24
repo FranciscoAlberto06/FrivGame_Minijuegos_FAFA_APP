bb2d7e61cb650d45da08b4e94c2e2799c37dd9ae
Francisco Alberto Fernández Alcántara <francisfdez591@gmail.com>
2026-04-24 13:56:05 +0200

    UpdateV8
    Agregamos nudget de MsqlConnection para conectarse a la bd en la nube, y creación de la api de Aiven con link de acceso

---
+++ BGestionFAFA/ApiAivenFAFA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace BGestionFAFA
+{
+    public class ApiAivenFAFA
+    {
+        // Link de conexión a la base de datos en Aiven 
+        static string connectionString = "mysql-14c4eb7-finalproyect-dam.i.aivencloud.com;Port=15348;Database=defaultdb;Uid=avnadmin;Pwd=tu-REQUIRED;SslMode=Required";
+
+        // Metodo que va cargar todo los datos nuevo que no tenga sqlite 
+        public static void CargarDatosNuevosDesdeAiven()
+        {
+            MySqlConnection conexion = new MySqlConnection(connectionString);
+
+            // Ahora vamos a pasar todos lo datos que hay nuestro sqlite a la nube, para que se guarden ahi y se puedan recuperar en otros dispositivos
+            // se haciendo lo siguiente: 
+
+
+        }
+    }
+}
